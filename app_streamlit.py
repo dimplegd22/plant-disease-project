e1f1048a50dc281bfae3f43cc9ebe775901a27dd
dimplegd22 <dimplegd2006@gmail.com>
2026-03-23 15:21:32 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -22,16 +22,25 @@ CLASS_LABELS = [
 class PlantDiseaseCNN(nn.Module):
     def __init__(self):
         super(PlantDiseaseCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3)
+
+        # MATCHING YOUR MODEL
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=4)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.fc1 = nn.Linear(32 * 127 * 127, len(CLASS_LABELS))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4)
+
+        # Fully connected
+        self.fc1 = nn.Linear(57600, 512)
+        self.fc2 = nn.Linear(512, len(CLASS_LABELS))
 
     def forward(self, x):
         x = self.pool(torch.relu(self.conv1(x)))
         x = self.pool(torch.relu(self.conv2(x)))
+
         x = x.view(x.size(0), -1)
-        x = self.fc1(x)
+
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+
         return x
 
 # ---------------------------
